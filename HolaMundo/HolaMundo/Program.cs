ef00304d32178862765a10f15fcfcb822dd5f4d4
rafagardo <rafaelgarcia1205@gmail.com>
2024-12-01 22:22:32 -0600

    HolaMundo first program

---
+++ HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Hola Rafael");
+            Console.Read();
+        }
+    }
+}
